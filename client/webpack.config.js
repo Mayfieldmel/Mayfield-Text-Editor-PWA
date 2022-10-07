bdeed703d6024b9065333bc1a312515ba588efb5
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-07 11:23:41 -0400

    add webpack plugins to webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,30 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+          swSrc: './src/sw.js',
+          swDest: 'service-worker.js',
+        }),
+        new WebpackPwaManifest({
+          name: 'Contact Cards Application',
+          short_name: 'Contact Cards',
+          description: 'Keep track of contacts!',
+          background_color: '#272822',
+          theme_color: '#31a9e1',
+          start_url: './',
+          publicPath: './',
+          icons: [
+            {
+              src: path.resolve('src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'),
+            }
+          ],
+        }),
     ],
 
     module: {
